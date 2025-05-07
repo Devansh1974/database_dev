63cfa5179ab84ceffac5d340f189d4839ad06731
Devansh singh <devanshsingh1974@gmail.com>
2025-05-07 23:30:09 +0530

    add

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,8 @@ import User from '../models/User.js';
 export const registerUser = async (req, res) => {
   try {
     const { username, email, role } = req.body;
-    if (!username || !email || !role) return res.status(400).json({ error: 'Missing fields' });
+    if (!username || !email || !role) 
+      return res.status(400).json({ error: 'Missing fields' });
 
     const newUser = new User({ username, email, role });
     await newUser.save();
@@ -11,4 +12,4 @@ export const registerUser = async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-};
+}; 
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@ cd pet-care-backend
 npm init -y
 npm install express mongoose dotenv
 npm install --save-dev nodemon
+TO Run - npm start
